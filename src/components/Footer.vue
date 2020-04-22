40e31ebf1a223060a770171709519f110f7036f2
fajargema <fgemar72@gmail.com>
2020-04-22 22:03:07 +0700

    Pembaruan tampilan home

---
+++ src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- Footer Section Begin -->
+  <footer class="footer-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="footer-left text-left">
+            <div class="footer-logo">
+              <a href="#">
+                <img src="img/logo_website_shayna_white.png" alt />
+              </a>
+            </div>
+            <ul>
+              <li>Address: Jamblan, Cirebon</li>
+              <li>Phone: +62 851-5513-765</li>
+              <li>Email: cs@fajargema.my.id</li>
+            </ul>
+            <div class="footer-social">
+              <a href="#">
+                <i class="fa fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fa fa-pinterest"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5 offset-lg-2">
+          <div class="newslatter-item">
+            <h5 class="text-left">Join Our Newsletter Now</h5>
+            <p class="text-left">
+              Get E-mail updates about our latest shop and special offers.
+            </p>
+            <form action="#" class="subscribe-form">
+              <input type="text" placeholder="Enter Your Mail" />
+              <button type="button">Subscribe</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright-reserved">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              Copyright &copy; 2020 All rights reserved | Shayna
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer Section End -->
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
